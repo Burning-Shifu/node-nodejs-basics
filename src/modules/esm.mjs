7ae90ebb0e48860a4f06f91eee365dc8ceea3229
Shifu <kris.deniso@mail.ru>
2023-06-19 13:58:26 -0600

    refactor: convert cjsToEsm.cjs to esm.mjs

--- src/modules/esm.mjs
+++ src/modules/esm.mjs
@@ -1,17 +1,23 @@
-const path = require('path');
-const { release, version } = require('os');
-const { createServer: createServerHttp } = require('http');
-require('./files/c');
+import { fileURLToPath } from 'url';
+import path from 'path';
+import { release, version } from 'os';
+import { createServer as createServerHttp } from 'http';
+import a from './files/a.json' assert { type: 'json' };
+import b from './files/b.json' assert { type: 'json' };
+import './files/c.js';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const random = Math.random();
-
 let unknownObject;
 
 if (random > 0.5) {
-    unknownObject = require('./files/a.json');
+    unknownObject = a;
 } else {
-    unknownObject = require('./files/b.json');
+    unknownObject = b;
 }
+console.log(unknownObject);
 
 console.log(`Release ${release()}`);
 console.log(`Version ${version()}`);
@@ -26,15 +32,9 @@ const myServer = createServerHttp((_, res) => {
 
 const PORT = 3000;
 
-console.log(unknownObject);
-
 myServer.listen(PORT, () => {
     console.log(`Server is listening on port ${PORT}`);
     console.log('To terminate it, use Ctrl+C combination');
 });
 
-module.exports = {
-    unknownObject,
-    myServer,
-};
-
+export {unknownObject, myServer};
