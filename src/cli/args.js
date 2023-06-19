6c2e2b516a45a525df7b6ec87e5a15e2fea8e1b2
Shifu <kris.deniso@mail.ru>
2023-06-19 02:21:52 -0600

    feat: add cli functions

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,21 @@
 const parseArgs = () => {
-    // Write your code here 
+  // Get command line arguments, excluding the first two
+  const args = process.argv.slice(2);
+  const argPairs = [];
+
+  // Loop over the arguments
+  for (let i = 0; i < args.length; i += 2) {
+    // Remove the leading '--' from the argument name
+    const argName = args[i].replace('--', '');
+
+    // Get the value of the argument
+    const argValue = args[i + 1];
+
+    // collect the argument and its value
+    argPairs.push(`${argName} is ${argValue}`);
+  }
+
+  console.log(argPairs.join(', '));
 };
 
 parseArgs();
--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,16 @@
 const parseEnv = () => {
-    // Write your code here 
+  const envVars = [];
+
+  // Loop over the process.env object
+  for (let key in process.env) {
+    // Check if the key starts with "RSS_"
+    if (key.startsWith("RSS_")) {
+      // collect key and value
+      envVars.push(`${key}=${process.env[key]}`);
+    }
+  }
+
+  console.log(envVars.join("; "));
 };
 
 parseEnv();
