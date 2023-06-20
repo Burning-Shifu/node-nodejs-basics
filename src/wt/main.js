19f6cadc041aca2c9e345c4597573be461aeef52
Shifu <kris.deniso@mail.ru>
2023-06-20 02:37:53 -0600

    feat: add worker-threads functions

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,5 +1,38 @@
+import { Worker } from 'worker_threads';
+import os from 'os';
+import { dirname, join } from 'path';
+import { fileURLToPath } from 'url';
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
 const performCalculations = async () => {
-    // Write your code here
+  // Get the number of CPU cores
+  const cpuCount = os.cpus().length;
+
+  // An array to hold all promises (one per worker)
+  const promises = [];
+
+  // Create as many workers as there are CPU cores
+  for (let i = 0; i < cpuCount; i++) {
+    promises.push(new Promise((resolve, reject) => {
+      const worker = new Worker(join(__dirname, 'worker.js'));
+
+      worker.once('message', (result) => {
+        resolve({ status: 'resolved', data: result });
+      });
+
+      worker.once('error', (error) => {
+        console.error(`Worker error: ${error}`);
+        resolve({ status: 'error', data: null });
+      });
+
+      worker.postMessage(i + 10);
+    }));
+  }
+
+  // Wait for all workers to finish and gather results
+  const results = await Promise.all(promises);
+  console.log(results);
 };
 
-await performCalculations();
+performCalculations().catch(console.error);
